b5d15748af94081bdaf85a457458969b38bcac16
Brianna Ratt <briannaratt@users.noreply.github.com>
2020-09-14 17:42:57 +0300

    kolmas tund lõppenud

--- tund3/home.php
+++ tund3/home.php
@@ -3,14 +3,6 @@
  $fulltimenow = date("d.m.Y H:i:s");
  $hournow = date("H");
  $partofday = "lihtsalt aeg";
- $weekdaynameset = ["esmaspäev", "teisipäev", "kolmapäev", "neljapäev", "reede", "laupäev", "pühapäev"];
- $monthnameset = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni",
- "juuli", "august", "september", "oktoober", "november", "detsember"];
- 
- //küsime nädalapäeva
- $weekdaynow = date("N");
- //echo $weekdaynow;
- 
  if($hournow < 6){
 	 $partofday = "uneaeg";
  }
@@ -20,25 +12,17 @@
  if($hournow >= 8 and $hournow < 18) {
 	 $partofday = "õppimise aeg";
  }
- if($hournow >= 19 and $hournow < 21) {
-	 $partofday = "puhkamise aeg";
- }
- if($hournow > 21) {
-	 $partofday = "uneaeg";
+if($hournow >= 19 and $hournow < 21) {
  }
  
  //jälgime semestri kulgu
  $semesterstart = new DateTime("2020-8-31");
  $semesterend = new DateTime("2020-12-13");
  $semesterduration = $semesterstart->diff($semesterend);
- $semesterdurationdays = $semesterduration->format("%r%a");
  $today = new DateTime("now");
  $fromsemesterstart = $semesterstart->diff($today);
  //saime aja erinevuse objektina, seda niisama näidata ei saa
  $fromsemesterstartdays = $fromsemesterstart->format("%r%a");
- $alltimesemester = $semesterstart->diff($semesterend);
- $alltimesemesterdays = $alltimesemester->format("%r%a");
- $dayspercentage = round($fromsemesterstartdays / $alltimesemesterdays * 100);
  
 ?>
 
@@ -54,13 +38,7 @@
   <h1><?php echo $username; ?> programmeerib veebi</h1>
   <p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
   <p>Leht on loodud veebiprogrammeerimise kursusel <a href="http://www.tlu.ee">Tallinna Ülikooli</a> Digitehnoloogiate instituudis<p>
-  <p>Lehe avamise aeg: <?php echo $weekdaynameset[$weekdaynow - 1] .", " . $fulltimenow .". Semestri algusest on möödunud " .$fromsemesterstartdays ." päeva"; ?>.
+  <p>Lehe avamise aeg: <?php echo $fulltimenow .", semestri algusest on möödunud " .$fromsemesterstartdays ." päeva"; ?>.
   <?php echo "Parajasti on " .$partofday ."."; ?></p>
-  <p>Semestri kestus päevades: <?php echo $alltimesemesterdays; ?></p>
-  <p>Semestrist on läbitud <?php echo $dayspercentage; ?> %!</p>
-  
 </body>
 </html>
-
-
-
