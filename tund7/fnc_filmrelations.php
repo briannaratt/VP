3dbeeff05de992c0e66ea090099802512d8e0f12
Brianna Ratt <briannaratt@users.noreply.github.com>
2020-10-26 10:38:18 +0200

    kodutööd 9.tund

---
+++ tund7/fnc_filmrelations.php
@@ -0,0 +1,105 @@
+<?php
+$database = "if20_brianna_3";
+
+function readstudiotoselect($selectedstudio){
+	$notice = "<p>Kahjuks stuudioid ei leitud!</p> \n";
+	$conn = new mysqli($GLOBALS["serverhost"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
+	$stmt = $conn->prepare("SELECT production_company_id, company_name FROM production_company");
+	echo $conn->error;
+	$stmt->bind_result($idfromdb, $companyfromdb);
+	$stmt->execute();
+	$studio= "";
+	while($stmt->fetch()){
+		<option value="1" selected>Siin me oleme</option>
+		$studios .= '<option value="' .$idfromdb .'"';
+		if($idfromdb == $selectedstudio){
+			$studios .=" selected";
+		}
+		$studios .= ">" .$companyfromdb ."</option> \n";
+	}
+	if(!empty($studios)){
+		$notice = '<select name="filmstudioinput" id="filmstudioinput">' ."\n";
+		$notice .= '<option value="" selected disabled>Vali stuudio/tootja</option>' ."\n";
+		$notice .= $studios;
+		$notice .= "</select> \n";
+	}
+}
+
+function readmovietoselect($selected){
+	$notice = "<p>Kahjuks filme ei leitud!</p> \n";
+	$conn = new mysqli($GLOBALS["serverhost"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
+	$stmt = $conn->prepare("SELECT movie_id, title FROM movie");
+	echo $conn->error;
+	$stmt->bind_result($idfromdb, $titlefromdb);
+	$stmt->execute();
+	$films = "";
+	while($stmt->fetch()){
+		$films .= '<option value="' .$idfromdb .'"';
+		if(intval($idfromdb) == $selected){
+			$films .=" selected";
+		}
+		$films .= ">" .$titlefromdb ."</option> \n";
+	}
+	if(!empty($films)){
+		$notice = '<select name="filminput" id="filminput">' ."\n";
+		$notice .= '<option value="" selected disabled>Vali film</option>' ."\n";
+		$notice .= $films;
+		$notice .= "</select> \n";
+	}
+	$stmt->close();
+	$conn->close();
+	return $notice;
+}
+
+function readgenretoselect($selected){
+	$notice = "<p>Kahjuks žanre ei leitud!</p> \n";
+	$conn = new mysqli($GLOBALS["serverhost"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
+	$stmt = $conn->prepare("SELECT genre_id, genre_name FROM genre");
+	echo $conn->error;
+	$stmt->bind_result($idfromdb, $genrefromdb);
+	$stmt->execute();
+	$genres = "";
+	while($stmt->fetch()){
+		$genres .= '<option value="' .$idfromdb .'"';
+		if(intval($idfromdb) == $selected){
+			$genres .=" selected";
+		}
+		$genres .= ">" .$genrefromdb ."</option> \n";
+	}
+	if(!empty($genres)){
+		$notice = '<select name="filmgenreinput" id="filmgenreinput">' ."\n";
+		$notice .= '<option value="" selected disabled>Vali žanr</option>' ."\n";
+		$notice .= $genres;
+		$notice .= "</select> \n";
+	}
+	$stmt->close();
+	$conn->close();
+	return $notice;
+}
+
+function storenewrelation($selectedfilm, $selectedgenre){
+	$notice = "";
+	$conn = new mysqli($GLOBALS["serverhost"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
+	$stmt = $conn->prepare("SELECT movie_genre_id FROM movie_genre WHERE movie_id = ? AND genre_id = ?");
+	echo $conn->error;
+	$stmt->bind_param("ii", $selectedfilm, $selectedgenre);
+	$stmt->bind_result($idfromdb);
+	$stmt->execute();
+	if($stmt->fetch()){
+		$notice = "Selline seos on juba olemas!";
+	} else {
+		$stmt->close();
+		$stmt = $conn->prepare("INSERT INTO movie_genre (movie_id, genre_id) VALUES(?,?)");
+		echo $conn->error;
+		$stmt->bind_param("ii", $selectedfilm, $selectedgenre);
+		if($stmt->execute()){
+			$notice = "Uus seos edukalt salvestatud!";
+		} else {
+			$notice = "Seose salvestamisel tekkis tehniline tõrge: " .$stmt->error;
+		}
+	}
+	
+	$stmt->close();
+	$conn->close();
+	return $notice;
+}
---
+++ tund7ja8/addideas.php
@@ -0,0 +1,58 @@
+<?php
+  session_start();
+  
+  //kui pole sisseloginud
+  if(!isset($_SESSION["userid"])){
+	  //jõugu sisselogimise lehele
+	  header("Location: page.php");
+  }
+  //väljalogimine
+  if(isset($_GET["logout"])){
+	  session_destroy();
+	   header("Location: page.php");
+	   exit();
+  }
+  //loeme andmebaasi login ifo muutujad
+  require("../../../config.php");
+  //kui kasutaja on vormis andmeid saatnud, siis salvestame andmebaasi
+  $database = "if20_brianna_3";
+  if(isset($_POST["submitnonsense"])){
+	  if(!empty($_POST["nonsense"])){
+		  //andmebaasi lisamine
+		  //loome andmebaasi ühenduse
+		  $conn = new mysqli($serverhost, $serverusername, $serverpassword, $database);
+		  //valmistame ette SQL käsu
+		  $stmt = $conn->prepare("INSERT INTO nonsens (nonsenseidea) VALUES(?)");
+		  echo $conn->error;
+		  //s - string, i -integer, d-decimal
+		  $stmt->bind_param("s", $_POST["nonsense"]);
+		  $stmt->execute();
+		  //käsk ja ühendus sulgeda
+		  $stmt->close();
+		  $conn->close();
+	  } 
+  }
+  
+  //$username = "Brianna Ratt";
+  
+  require("header.php");
+?>
+
+  <img src="../img/vp_banner.png" alt="Veebiprogrammeerimise kursuse bänner">
+  <h1><?php echo $_SESSION["userfirstname"] ." " .$_SESSION["userlastname"]; ?> programmeerib veebi</h1>
+  <p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <p>Leht on loodud veebiprogrammeerimise kursusel <a href="http://www.tlu.ee">Tallinna Ülikooli</a> Digitehnoloogiate instituudis.</p>
+  
+  <ul>
+    <li><a href="home.php">Avalehele</a></li>
+	<li><a href="?logout=1">Logi välja</a>!</li>
+  </ul>
+  
+  <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    <label>Sisesta oma tänane mõttetu mõte!</label>
+	<input type="text" name="nonsense" placeholder="mõttekoht">
+	<input type="submit" value="Saada ära!" name="submitnonsense">
+  </form>
+  
+</body>
+</html>
